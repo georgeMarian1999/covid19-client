189b305b174690121bc73510faa0a30946e19482
Anamaria Arseniuc <anamariaarseniuc@artsoft.ro>
2021-08-16 10:55:24 +0300

    medic page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import {Redirect, Route, Switch} from "react-router";
 import AdminPage from "./AdminPage/AdminPage";
+import MedicPage from "./MedicPage/MedicPage";
 
 function App() {
   return (
     <Switch>
         <Route exact path='/adminboard'>
             <AdminPage/>
+        </Route>
+        <Route path='/medicboard'>
+            <MedicPage/>
         </Route>
         <Route path={'*'} render={()=> <Redirect to={'/'}/>}/>
     </Switch>
