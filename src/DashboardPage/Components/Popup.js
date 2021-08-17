0de562d00ca3b3eda7ea8c74172882411cb21aa0
Oana Gurzau <oanagurzau@artsoft.ro>
2021-08-17 11:06:55 +0300

    WIP dashboard page

--- src/DashboardPage/Components/Popup.js
+++ src/DashboardPage/Components/Popup.js
@@ -1,13 +1,28 @@
-import React from "react";
+import { count } from "d3";
+import React, {useEffect} from "react";
+import {useSelector} from "react-redux";
+import style from "./Popup.module.css"
 
-const County = ({county_code,counties}) => {
-    const index = counties.findIndex(el => el.county_code === county_code);
+
+const Popup = ({county_code}) => {
+    const counties = useSelector(state => state.counties);
+    const [index, setIndex] = React.useState(0) 
+    useEffect(() => {
+        console.log(counties);
+
+        console.log(county_code, typeof county_code)
+        console.log(counties.findIndex(el => el.county_code == county_code))
+      setIndex(counties.findIndex(el => el.county_code == county_code))
+    }, []);
+  
+    
+    // const index = counties.findIndex(el => el.county_code === county_code);
     return(
-        <div>
-            {counties[index].county}
+        <div className={style.infos}>
+            {/* {counties[index].county} */}
         </div>
     )
 }
 
 
-export default County;
+export default Popup;
