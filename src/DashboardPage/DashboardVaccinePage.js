d0110e754079df74c10b15797c249c65ef980dc6
georgeMarian1999 <mariangeorge99@icloud.com>
2021-08-18 16:22:12 +0300

    Doses by age and doses by gender added

--- src/DashboardPage/DashboardVaccinePage.js
+++ src/DashboardPage/DashboardVaccinePage.js
@@ -2,38 +2,46 @@ import React, {useEffect, useState} from 'react';
 import VaccineMap from "./VaccineComponents/VaccineMap";
 import ONLINE_URL from "../Common/ONLINE_URL";
 import axios from 'axios';
-import DynamicGraph from "./VaccineComponents/DynamicGraph";
+import DosesByAgeGraph from "./VaccineComponents/DosesByAgeGraph";
+import DosesByGender from "./VaccineComponents/DosesByGender";
 const DashboardVaccinePage = () =>{
     const [vaccineData,setVaccineData] = useState([]);
     const [vaccineByRisk,setVaccineByRisk] = useState([]);
+    const [vaccineByGender,setVaccineByGender] = useState([]);
 
 
     useEffect(()=>{
         const promise1 = getVaccineData();
         const promise2 = getVaccineDataByRiskGroup();
+        const promise3 = getVaccineDataByGender();
 
-        axios.all([promise1,promise2])
+        axios.all([promise1,promise2,promise3])
             .then(axios.spread((...responses)=>{
                 const response1 = responses[0];
                 setVaccineData(response1.data);
                 const response2 = responses[1];
                 setVaccineByRisk(response2.data);
-                console.log(response1.data);
+                const response3 = responses[2];
+                setVaccineByGender(response3.data);
             }))
             .catch(err=>{
                 console.log(err);
             })
-    })
+    },[])
     const getVaccineData = () =>{
        return axios.get(ONLINE_URL+'dosesByCounty');
     }
     const getVaccineDataByRiskGroup = () =>{
         return axios.get(ONLINE_URL+'vaccine/age');
     }
+    const getVaccineDataByGender = () =>{
+        return axios.get(ONLINE_URL+'vaccine/gender');
+    }
     return(
         <>
          <VaccineMap vaccineData={vaccineData}/>
-            <DynamicGraph vaccineByRisk={vaccineByRisk}/>
+            <DosesByAgeGraph vaccineByRisk={vaccineByRisk}/>
+            <DosesByGender vaccineByGender={vaccineByGender}/>
         </>
     )
 };
